9b92843da74ef78ffc617d5353a9cfe728f8e2b1
EugeneSolovey <birdman555000@gmail.com>
2022-06-11 17:01:54 +0300

    Modificated

--- Home_work/HomeWork10.py
+++ Home_work/HomeWork10.py
@@ -3,26 +3,18 @@ import random
 num1 = int(input("Enter number: "))
 list1 = [random.randint(0, num1) for i in range(num1)]
 
-print(list1)
+# print(list1)
 
-count = 0
 num2 = int(input("Enter number: "))
 
 for i, num in enumerate(list1):
- #   print(i, num, end=', ')
-  #  print()
-    if list1[count] + num == num2:
-        if i > 0 and count == i:
-            count += 1
-            continue
-        print(list1[count], num)
-        print('Index of numbers whose sum is equal to the entered: ', count, 'and', i)
-        break
-  #  elif list1[count] + num != num2:
-  #      print( list1[count], num )
-   #     count += 1
-    elif count == num1 - 1:
-        print('There are no numbers in the list whose sum is equal to the one entered!')
+    for j in range(i+1, len(list1)):
+        if list1[i] + list1[j] == num2:
+            print('Index of numbers whose sum is equal to the entered: ', i, 'and', j)
+            break
+
+    if list1.index(num) == len(list1) - 1:
+        print('There are no numbers in the list whose sum is equal to the one entered')
         break
 
 input()
