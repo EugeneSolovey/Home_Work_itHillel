41f1cd0e5cbd7f7073e98b65737b2dd44a691837
EugeneSolovey <birdman555000@gmail.com>
2022-06-23 06:57:05 +0300

    Add file with homework #15 to ninth lesson

--- Home_work/HomeWork15.py
+++ Home_work/HomeWork15.py
@@ -1,19 +1,16 @@
-user_input = int( input( 'Enter number: ' ) )
+user_input = int(input('Enter number: '))
 
 previous = correct = 1
 
 if user_input >= 2:
     for _ in range((user_input - 2)):
         previous, correct = correct, previous + correct
-    print( 'Fibonacci number for', str( user_input ), 'is', str( correct ), '- through the cycle' )
+    print('Fibonacci number for', str(user_input), 'is', str(correct), '- through the cycle')
 else:
     print('Fibonacci number for', str(user_input), 'is', str(user_input), '- through the cycle')
 
 
-
-
 def fibo_numb(num: int) -> int:
-    correct2 = 1
     if num >= 2:
         correct2 = fibo_numb(num - 1) + fibo_numb(num - 2)
     else:
