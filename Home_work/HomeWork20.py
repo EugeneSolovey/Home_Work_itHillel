64f976d504daa1c904ccc116acd785dd0d4391e6
EugeneSolovey <birdman555000@gmail.com>
2022-07-16 18:55:14 +0300

    Add file with homework #20 to fourteen lesson

--- Home_work/HomeWork20.py
+++ Home_work/HomeWork20.py
@@ -1,52 +1,58 @@
-'''Реализовать класс счетчика. В классе реализовать:
-установки максимального значения (так же начального значения счётчика) при инициализации
-увеличения счетчика на 1
-возвращения текущего значения счётчика
-методы сравнения и равенства двух счетчиков'''
+minimal1 = int(input('Enter the minimum value Counter#1: '))
+maximal1 = int(input('Enter the maximum value Counter#1: '))
+minimal2 = int(input('Enter the minimum value Counter#2: '))
+maximal2 = int(input('Enter the maximum value Counter#2: '))
 
 class Counter:
-    current=0
-    def __init__(self, start: int, end: int):
-        self.start = start
-        self.end = end
 
-    def increase(self):
+    def __init__(self, minimal: int, maximal: int):
+        self.minimal = minimal
+        self.maximal = maximal
+        self.current = minimal
 
-        if self.start < self.end:
-            self.start += 1
-            return self.start
-        else:
-            return 'Out of range'
+    def increment(self):
+        self.current += 1
+        if self.current > self.maximal:
+            self.current = self.minimal
 
-
-class CounterMetods(Counter):
     def __repr__(self):
-        return f'{self.start=} {self.end=}'
+        return f'Counter({self.minimal=}, {self.maximal=}), current position={self.current=}'
 
-    def __eq__(self):
-        return self.start == self.current
+    def __eq__(self, other):
+        return self.current == other.current
 
-    def __lt__(self):
-        return self.start < self.current
+    def __lt__(self, other):
+        return self.current < other.current
 
-    def __gt__(self):
-        return self.start > self.current
-
-    def __le__(self):
-        return self.start <= self.current
-
-    def __ge__(self):
-        return self.start >= self.current
-
-    def __ne__(self):
-        return self.start != self.current
+    def __gt__(self, other):
+        return self.current > other.current
 
 
-my_counter1 = CounterMetods(start=5, end=20)
+if __name__ == '__main__':
+    counter1 = Counter(minimal=minimal1, maximal=maximal1)
+    counter2 = Counter(minimal=minimal2, maximal=maximal2)
+    print(counter1)
+    print(counter2)
+    if maximal1 > maximal2:
+        if minimal1 < minimal2:
+            llen = maximal1 - minimal1
+        else:
+            llen = maximal1 - minimal2
+    else:
+        if minimal1 < minimal2:
+            llen = maximal2 - minimal1
+        else:
+            llen = maximal2 - minimal2
+    for _ in range(llen):
+        counter1.increment()
+        counter2.increment()
+        print(f'Counter#1 is equals Counter#2 - {counter1 == counter2}', f'{counter1.current=}, {counter2.current=}')
+        print(f'Counter#1 is less than Counter#2 - {counter1 < counter2}', f'{counter1.current=}, {counter2.current=}')
+        print(f'Counter#1 is greater than Counter#2 - {counter1 > counter2}', f'{counter1.current=}, {counter2.current=}')
+    print('Check resetting')
+    counter1.increment()
+    counter2.increment()
+    print( counter1.current)
+    print( counter2.current)
 
-print(my_counter1.increase())
-print(my_counter1.increase())
-print(my_counter1.increase())
-print(my_counter1.increase())
-print(my_counter1.increase())
-
+print()
